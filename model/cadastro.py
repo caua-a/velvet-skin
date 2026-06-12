25dd1fb53bbc1ebd815742ede6b22e31c3921a66
Cauã Gustavo <caua.g.silva16@aluno.senai.br>
2026-06-12 11:28:01 -0300

    Model

--- model/cadastro.py
+++ model/cadastro.py
@@ -1,11 +1,29 @@
 from database.conexao import conectar
 
+
+def recuperar(usuario):
+    conexao, cursor = conectar()
+    cursor.execute('SELECT * FROM usuarios WHERE usuario = %s', (usuario,))
+    user = cursor.fetchone()
+    
+    cursor.close()
+    conexao.close()
+    return user
+
+def recuperar_users(usuario, password):
+    conexao, cursor = conectar()
+    cursor.execute('SELECT usuario, senha FROM usuarios WHERE usuario = %s AND senha = %s', (usuario, password))
+    user = cursor.fetchone()
+    if user:
+        return user
+    else:
+        return []
+
+
 def cadastrar_usuario(usuario, password, email, telefone, endereco):
     conexao, cursor = conectar()
-    cursor.execute('SELECT * FROM usuario;')
-    num_users = cursor.fetchall()
     try:
-        cursor.execute('INSERT INTO usuario(usuario, senha, email, telefone, endereco_principal) VALUES(%s, %s, %s, %s, %s)', (usuario, password, email, telefone, endereco))
+        cursor.execute('INSERT INTO usuarios(usuario, senha, email, telefone, endereco_principal) VALUES(%s, %s, %s, %s, %s)', (usuario, password, email, telefone, endereco))
         conexao.commit()
         conexao.close()
         return True
